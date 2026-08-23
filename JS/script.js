2f6bca1aa903a90183c405f8fb3532d2daf494e6
Christopher BM <bamc1715@gmail.com>
2026-08-23 12:38:09 -0600

    Primera implementacion de la IA para alinear el texto mediante bootstrap

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 // Si le gusta el anime dar una recomendacion
 
-const year = prompt("ingrese su año de nacimiento");
-const name = prompt("ingrese su nombre: ");
-const lastname = prompt("ingrese su apellido: ");
-let edad = 2026 - year;
-alert("hola " + name + lastname + " tu año de nacimiento es " + year + " y tu edad es " + edad + " años");
+// const year = prompt("ingrese su año de nacimiento");
+// const name = prompt("ingrese su nombre: ");
+// const lastname = prompt("ingrese su apellido: ");
+// let edad = 2026 - year;
+// alert("hola " + name + lastname + " tu año de nacimiento es " + year + " y tu edad es " + edad + " años");
 
-if (edad >= 18) {
-    alert("Puedes ver estas series ");
-} else {
-    alert("Aun que no contine contenido explicitos se recomiendo esperar a los 18 para disfrutar mejor del contenido");
-}
+// if (edad >= 18) {
+//     alert("Puedes ver estas series ");
+// } else {
+//     alert("Aun que no contine contenido explicitos se recomiendo esperar a los 18 para disfrutar mejor del contenido");
+// }
